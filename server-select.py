b695c249332a691e9810565a5d8634e8d4401e91
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2026-03-15 09:00:03 +0700

    feat: add improvement for sync and select
    - better file name handling and security
    - remove broadcast feature on sync version
    - detects when a client disconnected (non-sync)

--- server-select.py
+++ server-select.py
@@ -1,6 +1,7 @@
 import socket
 import logging
 import struct
+import select
 import os
 
 logging.basicConfig(level=logging.INFO)
@@ -45,7 +46,7 @@ def send_file_chunked(sock, path):
             sock.sendall(struct.pack(">I", len(chunk)) + chunk)
     sock.sendall(struct.pack(">I", 0)) # Sentinel value
 
-def handle_command(cmd_data, connection):
+def handle_command(cmd_data, connection, all_socket, server_socket):
     if cmd_data.startswith("/list"):
         if not os.path.isdir("storage"):
             send_msg(connection, "None")
@@ -68,6 +69,7 @@ def handle_command(cmd_data, connection):
         if not filename:
             logging.info(f"Invalid Filename: {raw_filename}")
             send_msg(connection, f"ERR_INVALID_FILENAME: {raw_filename}")
+            # broadcast_message(f"[BROADCAST] ERR_INVALID_FILENAME: {raw_filename}", connection, all_socket, server_socket)
             return
 
         filepath = os.path.join("storage", filename)
@@ -94,13 +96,22 @@ def handle_command(cmd_data, connection):
             if not filename:
                 logging.info(f"Invalid Filename: {raw_filename}")
                 send_msg(connection, f"ERR_INVALID_FILENAME: {raw_filename}")
+                # broadcast_message(f"[BROADCAST] ERR_INVALID_FILENAME: {raw_filename}", connection, all_socket, server_socket)
                 return
 
             filepath = os.path.join("storage", filename)
             logging.info(f"Receiving file: {filename}")
             recv_file_chunked(connection, filepath)
             send_msg(connection, f"Successfully uploaded file: {filename}")
+            broadcast_message(f"New file uploaded: {filename}", connection, all_socket, server_socket)
 
+def broadcast_message(message, sender_socket, all_socket, server_socket):
+    for sock in all_socket:
+        if sock is not server_socket and sock is not sender_socket:
+            try:
+                send_msg(sock, f"[BROADCAST] {message}")
+            except:
+                pass
 
 def filter_filename(filename):
     basename = os.path.basename(filename)
@@ -108,26 +119,49 @@ def filter_filename(filename):
         return None
     return basename
 
-def start_sync_server(host='127.0.0.1', port=5000):
+def start_select_server(host='127.0.0.1', port=5000):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((host, port))
     server.listen(5)
+    server.setblocking(False)
 
     logging.info(f"Server is listening at address {host} on port {port}")
 
+    client_sockets = [server]
+
     try:
         while True:
-            logging.info("waiting for connection")
-            connection, client_address = server.accept()
-            logging.info(f"connection from {client_address}")
+            socket_ready, _, _ = select.select(client_sockets, [], [])
 
-            while True:
-                cmd_data = recv_msg(connection)
-                if not cmd_data:
-                    break
+            for s in socket_ready:
+                if s is server:
+                    connection, client_addr = server.accept()
+                    connection.setblocking(False)
+                    client_sockets.append(connection)
+    
+                    logging.info(f"Connected: {client_addr}")
+                    broadcast_message(f"User {client_addr} joined.", connection, client_sockets, server)
+                else:
+                    try:
+                        cmd_data = recv_msg(s)
+                        if cmd_data:
+                            handle_command(cmd_data, s, client_sockets, server)
+                        else:
+                            try:
+                                addr = s.getpeername()
+                            except:
+                                addr = "Unknown"
 
-                handle_command(cmd_data, connection)
+                            logging.info(f"Client {addr} disconnected")
+
+                            if s in client_sockets:
+                                client_sockets.remove(s)
+                            s.close()
+
+                            broadcast_message(f"User {addr} has left.", None, client_sockets, server)
+                    except Exception as ee:
+                        logging.info(f"ERROR: {str(ee)}")
 
     except Exception as ee:
         logging.info(f"ERROR: {str(ee)}")
@@ -139,4 +173,4 @@ def start_sync_server(host='127.0.0.1', port=5000):
         server.close()
 
 if __name__ == '__main__':
-    start_sync_server()
+    start_select_server()
